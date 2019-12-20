0cedc2ff962f15c8146dbb4d056bc0ee42b4ca70
Antonio <ksion4ik.life@gmail.com>
2019-12-21 00:36:20 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.ba-slaider').slick({
+        slidesToShow: 1,
+        dots: true,
+    prevArrow: "<img src='img/arrow_right.png' class='prev' alt='1'>",
+    nextArrow: "<img src='img/arrow_left.png' class='next' alt='2'>",
+      });
+   
+     })
